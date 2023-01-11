844e623c2fd786b3d873a7e2c06e44e83df56bdc
eric.b.webb13.civ@mail.mil <eric.b.webb13.civ@mail.mil>
2023-01-10 20:28:56 -0500

    ver2

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,42 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Web3Service } from 'src/app/services/Web3/web3.service';
-import Web3 from 'web3';
+import { Component, OnInit } from "@angular/core";
+import { Web3Service } from "src/app/services/Web3/web3.service";
+// import Web3 from 'web3';
+import $toonCoinContract from "src/app/services/Solidity/$toon.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
-  userAddress: string = ''
-  isConnected: boolean = false
+  userAddress: string = "";
+  isConnected: boolean = false;
+  totalStakedNfts: number = 0;
 
-  constructor(private web3: Web3Service) {
-  }
+  constructor(private web3: Web3Service) {}
 
   async ngOnInit() {
+    this.totalStakedNfts = await updatedTotalStakedNfts();
+
+    {
+      setInterval(() => {
+        updatedTotalStakedNfts();
+      }, 100000);
+    }
   }
-
-  // async tryAndConnect(b: boolean) {
-  //   if(b == true){
-  //   try {
-  //     this.userAddress = await this.web3.getAccounts()
-  //     if(this.userAddress!= ''){
-  //       this.userAddress = Web3.utils.toChecksumAddress(this.userAddress[0])
-  //       this.isConnected= true
-  //     }
-
-  //   } catch (e) {
-  //     console.log(e)
-
-  //   }
-
-  //   }else{
-  //     this.userAddress = ''
-  //     this.isConnected= false
-  //   }
-
-  // }
 }
 
-
+async function updatedTotalStakedNfts() {
+  console.log("Test");
+  try {
+    let retVal = await $toonCoinContract.methods.stakedNfts().call();
+    return retVal;
+  } catch (e) {
+    console.log(e);
+  }
+}
