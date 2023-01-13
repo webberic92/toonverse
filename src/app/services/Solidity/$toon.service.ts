1978ec34784f08da108ef3f30ccb54d1993fc5bb
eric.b.webb13.civ@mail.mil <eric.b.webb13.civ@mail.mil>
2023-01-13 16:12:07 -0500

    test

---
+++ src/app/services/Solidity/$toon.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+const contract = require("../../../../solidity/coin/$toon.json");
+const Web3 = require('web3');
+//testNet
+// const provider = new Web3('https://mainnet.infura.io/v3/589e31a14b0c42b78400756b023d7894');
+const provider = new Web3('https://goerli.infura.io/v3/589e31a14b0c42b78400756b023d7894');
+//Binanace test net
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContractService {
+
+  constructor() { }
+}
+
+provider.eth.setProvider(Web3.givenProvider);
+
+  // '0x61DED8A72cDc7762D159ab46bE880BE7127A2DeF' // Live $TOON contract MAIN NET
+
+const $toonCoinContract = new provider.eth.Contract(
+  (contract),
+  "0x435dd9C7ADAcd74c666Cc00CD5Ca57452d1642B3"
+);
+
+
+export default $toonCoinContract;
