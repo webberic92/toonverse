58c447ffe3b70a82aa7cc84a6ba032ca5a729893
eric.b.webb13.civ@mail.mil <eric.b.webb13.civ@mail.mil>
2023-01-14 01:17:51 -0500

    website talking to gremlins.

--- src/app/components/fruittown/fruittown.component.ts
+++ src/app/components/fruittown/fruittown.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import fruitTown from "../../services/Solidity/fruitTown.service";
 import {ProviderLessNftContract} from "../../services/Solidity/devilCatz.service";
+import { Web3Service } from "src/app/services/Web3/web3.service";
 
 @Component({
   selector: 'app-fruittown',
@@ -9,12 +10,33 @@ import {ProviderLessNftContract} from "../../services/Solidity/devilCatz.service
 })
 export class FruittownComponent implements OnInit {
 
-  constructor() { }
+  constructor(private web3: Web3Service) { }
+
 
   fruitTownAddress: string = "";
+  userAddress: string = "";
+  hasStakedAddressClaimed: boolean = true;
 
-  ngOnInit(): void {
+
+  async ngOnInit(): Promise<void> {
     this.fruitTownAddress = fruitTown._address;
+    let addressArray = await this.web3.getAccounts();
+    this.userAddress = addressArray[0];
+    console.log(this.userAddress)
+    console.log( fruitTown.methods)
+
+    this.hasStakedAddressClaimed = await fruitTown.methods.hasStakerAddressFreeMinted(this.userAddress).call();
+
+
+    console.log(this.hasStakedAddressClaimed)
+
   }
 
+  async stakedAddressFreeMintClaim(){
+
+  await fruitTown.methods.stakedAddressesFreeMint().send({
+    from: this.userAddress
+  });
+
+  }
 }
